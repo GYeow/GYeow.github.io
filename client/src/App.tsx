7de0b4c49559df0b7b8d381b46b313f60c3e0107
Yeow <654405494@qq.com>
2026-01-30 03:55:06 -0800

    Implement clean URLs using 404 hack for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,15 +11,13 @@ import Projects from "@/pages/Projects";
 
 function Router() {
   return (
-    <WouterRouter hook={useHashLocation}>
-      <Switch>
-        <Route path="/" component={About} />
-        <Route path="/publications" component={Publications} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/cv" component={CV} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path="/" component={About} />
+      <Route path="/publications" component={Publications} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/cv" component={CV} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
